63f5ba846d66b104252d446b342cfe80eb16046d
scofiope <404196578@qq.com>
2020-03-13 02:54:40 +0800

    1

--- src/main/java/com/pger/de/template/course/Test.java
+++ src/main/java/com/pger/de/template/course/Test.java
@@ -3,13 +3,20 @@ package com.pger.de.template.course;
 public class Test {
     public static void main(String[] args) {
         System.out.printf("======== java架构师课程 ========");
-        JavaCourse java = new JavaCourse();
-        java.setNeedCheckHomework(false);
-        java.createCourse();
+//        JavaCourse java = new JavaCourse();
+//        java.setNeedCheckHomework(false);
+//        java.createCourse();
 
-        System.out.printf("======== Python 课程 =========");
-        PythonCourse python = new PythonCourse();
-        python.createCourse();
+//        System.out.printf("======== Python 课程 =========");
+//        PythonCourse python = new PythonCourse();
+//        python.createCourse();
 
+
+        CompositeJavaCourse java = new CompositeJavaCourse();
+        java.createJavaCourse();
+
+        System.out.printf("\n======== Python 课程 =========");
+        CompositePythonCourse python = new CompositePythonCourse();
+        python.createPythonCourse();
     }
 }
