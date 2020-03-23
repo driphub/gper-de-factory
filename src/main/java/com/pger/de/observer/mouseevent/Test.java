66d815f5b39dcc7daa99a2ea615dc89a187fd60d
scofiope <404196578@qq.com>
2020-03-23 23:31:54 +0800

    1

--- src/main/java/com/pger/de/observer/mouseevent/Test.java
+++ src/main/java/com/pger/de/observer/mouseevent/Test.java
@@ -1,4 +1,20 @@
 package com.pger.de.observer.mouseevent;
 
+import com.pger.de.observer.mouseevent.handler.Mouse;
+import com.pger.de.observer.mouseevent.handler.MouseEventListener;
+import com.pger.de.observer.mouseevent.handler.MouseEventType;
+
 public class Test {
+    public static void main(String[] args) {
+        MouseEventListener listener = new MouseEventListener();
+
+        Mouse mouse = new Mouse();
+
+        mouse.addLisenter(MouseEventType.ON_CLICK, listener);
+        mouse.addLisenter(MouseEventType.ON_MOVE, listener);
+
+        mouse.click();
+        mouse.move();
+
+    }
 }
--- src/main/java/com/pger/de/observer/mouseevent/handler/Mouse.java
+++ src/main/java/com/pger/de/observer/mouseevent/handler/Mouse.java
@@ -12,8 +12,46 @@ public class Mouse extends EventContext {
         this.trigger(MouseEventType.ON_CLICK);
     }
 
+    public void doubleClick() {
+        System.out.printf("调用双击方法");
+        this.trigger(MouseEventType.ON_DOUBLE_CLICK);
+    }
+
+    public void up() {
+        System.out.printf("调用弹起方法");
+        this.trigger(MouseEventType.ON_UP);
+    }
+
+    public void down() {
+        System.out.printf("调用按下方法");
+        this.trigger(MouseEventType.ON_DOWN);
+    }
+
     public void move() {
         System.out.println("调用移动方法");
         this.trigger(MouseEventType.ON_MOVE);
     }
+
+    public void wheel() {
+        System.out.printf("调用滚动方法");
+        this.trigger(MouseEventType.ON_WHEEL);
+    }
+
+    public void over() {
+        System.out.printf("调用悬停方法");
+        this.trigger(MouseEventType.ON_OVER);
+    }
+
+    public void blur() {
+        System.out.printf("调用失去焦点方法");
+        this.trigger(MouseEventType.ON_BLUR);
+    }
+
+    public void focus() {
+        System.out.printf("调用获取焦点方法");
+        this.trigger(MouseEventType.ON_FOCUS);
+    }
+
+
+
 }
