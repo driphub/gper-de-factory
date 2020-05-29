11cfa4171790e92f418d7a8c5e04e0f3c667fe56
scofiope <404196578@qq.com>
2020-05-29 11:40:45 +0800

    1

--- src/main/java/com/pger/de/chainres/auth/optimiaze/ValidateHandler.java
+++ src/main/java/com/pger/de/chainres/auth/optimiaze/ValidateHandler.java
@@ -6,6 +6,7 @@ import org.springframework.util.StringUtils;
 /**
  */
 public class ValidateHandler extends Handler {
+    @Override
     public void doHandler(Member member) {
         if(StringUtils.isEmpty(member.getLoginName()) ||
                 StringUtils.isEmpty(member.getLoginPass())){
